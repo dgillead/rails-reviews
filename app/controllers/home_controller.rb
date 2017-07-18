31114457eaa82047a26cd0dbc0f3b2da61f9f57d
Darnell Gillead <dlgillead@gmail.com>
2017-07-17 20:50:29 -0500

    added route to display registration form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def register
+    user = User.new
+    render :register, locals: { user: user }
+  end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -10,4 +10,15 @@ describe HomeController do
       expect(response.body).to include("Welcome")
     end
   end
+
+  describe "GET /register" do
+    it "renders the registration form" do
+      get :register
+
+      expect(response.body).to include("Register a New User")
+      expect(response.body).to include("Email")
+      expect(response.body).to include("Password")
+      expect(response.body).to include("Repeat Your Password")
+    end
+  end
 end
